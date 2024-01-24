ad21c6a282af23efe812fdc8ffadcf37c5254942
Anirban Bandyopadhyay <devxaves@gmail.com>
2024-01-24 15:45:48 +0530

    adding exams

---
+++ src/components/aspirants/UpcomingExams.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function UpcomingExams() {
+  return (
+    <div>UpcomingExams</div>
+  
+  )
+}
+
+export default UpcomingExams;
--- src/pages/Aspirants.jsx
+++ src/pages/Aspirants.jsx
@@ -10,19 +10,24 @@ function Aspirants(props) {
             <PageTitle title='For Aspirants' desc="Your Preparation Zone" />
             <div className="block-text center">
                 <h3 className='heading'>Upcoming Exams</h3>
+                
                 <div className="Upcoming_exams">
-                    <ul>
-                        <li>4 Feb-Gate- Gratitide Aptitude Test In Engineering</li>
-                        <li>4 Feb-Gate- Gratitide Aptitude Test In Engineering</li>
-                        <li>4 Feb-Gate- Gratitide Aptitude Test In Engineering</li>
-                        <li>4 Feb-Gate- Gratitide Aptitude Test In Engineering</li>
-
-                    </ul>
+                4 Feb - GATE - Gratitide Aptitude Test In Engineering
                 </div>
+                <div className="Upcoming_exams">
+                4 Feb - GATE - Gratitide Aptitude Test In Engineering
+                </div>
+                <div className="Upcoming_exams">
+                4 Feb - GATE - Gratitide Aptitude Test In Engineering
+                </div>
+                <div className="Upcoming_exams">
+                4 Feb - GATE - Gratitide Aptitude Test In Engineering
+                </div>
+               
             </div>
 
             <div className="block-text center">
-                <h3 className="heading">Video Resources</h3>
+                <h3 className="heading"  >Video Resources</h3>
             </div>
         </div>
 
