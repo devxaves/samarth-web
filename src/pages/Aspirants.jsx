db526bbf17a9ee131a92e7b40eee0267e165759e
Anirban Bandyopadhyay <devxaves@gmail.com>
2024-01-24 17:14:43 +0530

    added red dots

--- src/pages/Aspirants.jsx
+++ src/pages/Aspirants.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import "../scss/component/aspirants.scss"
+import img from "../assets/images/background/reddot.png"
 
 import PageTitle from '../components/pagetitle/PageTitle';
 
@@ -12,16 +13,16 @@ function Aspirants(props) {
                 <h3 className='heading'>Upcoming Exams</h3>
                 
                 <div className='exams-list' >
-                <div className="Upcoming_exams">
+                <div className="Upcoming_exams"><img className='reddot' src={img} alt="dot" />
                 3 Feb 2024 - GATE - Gratitide Aptitude Test In Engineering
                 </div>
-                <div className="Upcoming_exams">
+                <div className="Upcoming_exams"><img className='reddot' src={img} alt="dot" />
                 26 May 2024 - UPSC (prelims) - Union Public Service Commission
                 </div>
-                <div className="Upcoming_exams">
+                <div className="Upcoming_exams"><img className='reddot' src={img} alt="dot" />
                 20 Sept 2024 - UPSC (mains) - Union Public Service Commission
                 </div>
-                <div className="Upcoming_exams">
+                <div className="Upcoming_exams"><img className='reddot' src={img} alt="dot" />
                 June 2024 - WBCS - West Bengal Public Service Commission
                 </div>
                 </div>
